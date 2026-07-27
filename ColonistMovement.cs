670e2918b5ed043a5a706fcd430398d2819c1a71
Andrew Hulme <148535372+AndrewHulme88@users.noreply.github.com>
2026-07-27 16:08:09 +1000

    Finished basic colonist movement

--- ColonistMovement.cs
+++ ColonistMovement.cs
@@ -1,16 +1,51 @@
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
 
 public class ColonistMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private PathfindingGrid pathfindingGrid;
+    [SerializeField] private Tilemap groundTilemap;
+    [SerializeField] private float moveSpeed = 3f;
+
+    private Coroutine movementCoroutine;
+
+    public void MoveTo(Vector3Int targetGridPosition)
     {
-        
+        Vector3Int startGridPosition = groundTilemap.WorldToCell(transform.position);
+
+        List<Vector3Int> path = pathfindingGrid.FindPath(startGridPosition, targetGridPosition);
+
+        if (path == null || path.Count == 0)
+        {
+            Debug.LogWarning($"No path found to {targetGridPosition}.");
+
+            return;
+        }
+
+        if (movementCoroutine != null)
+            StopCoroutine(movementCoroutine);
+
+        movementCoroutine = StartCoroutine(FollowPath(path));
     }
 
-    // Update is called once per frame
-    void Update()
+    private IEnumerator FollowPath(List<Vector3Int> path)
     {
-        
+        for (int i = 1; i < path.Count; i++)
+        {
+            Vector3 targetWorldPosition = groundTilemap.GetCellCenterLocal(path[i]);
+
+            while (Vector3.Distance(transform.position, targetWorldPosition) > 0.01f)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, targetWorldPosition, moveSpeed * Time.deltaTime);
+
+                yield return null;
+            }
+
+            transform.position = targetWorldPosition;
+        }
+
+        movementCoroutine = null;
     }
 }
